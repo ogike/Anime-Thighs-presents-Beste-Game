ad3410279da1c4dc7220680e0ae30f9779ffb953
SagiMarton <38342862+SagiMarton@users.noreply.github.com>
2021-02-07 19:59:26 +0100

    kezdetleges sword swing

--- Assets/MyScripts/PlayerBehaviour/MeleeWeaponScript.cs
+++ Assets/MyScripts/PlayerBehaviour/MeleeWeaponScript.cs
@@ -10,19 +10,58 @@ using UnityEngine;
 public class MeleeWeaponScript : MonoBehaviour
 {
 
-    
+    enum Position
+    {
+        Left,
+        Right
+    }
+
+    bool IsAttacking;
+    Position myPos;
     public int damage = 200;
     public int knockbackStrength = 15;
+    public float swingCD = 0.3f;
+    public float curCD;
+    public float swingSpeed = 200;
+
+    Transform myTrans;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myPos = Position.Right;
+        curCD = 0;
+        myTrans = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (curCD > 0)
+        {
+            curCD -= Time.deltaTime;
+            if (myPos == Position.Right)
+            {
+
+                transform.RotateAround(transform.parent.position, Vector3.forward, swingSpeed * Time.deltaTime);
+                //myTrans.rotation = transform.parent.rotation * Quaternion.Euler(0, 0, 180);
+
+                
+            }
+            else
+            {
+
+                transform.RotateAround(transform.parent.position, Vector3.forward, swingSpeed * -1 * Time.deltaTime);
+                //myTrans.rotation = transform.parent.rotation * Quaternion.Euler(0, 0, 180);
+
+                
+            }
+        }
+        if (!(curCD > 0) && Input.GetMouseButton(0))
+        {
+            Attack();
+
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -40,4 +79,14 @@ public class MeleeWeaponScript : MonoBehaviour
 	{
         //update your stats here
 	}
+
+    void Attack()
+    {
+        curCD = swingCD;
+        if (myPos == Position.Right)
+            myPos = Position.Left;
+        else
+            myPos = Position.Right;
+    }
+
 }
