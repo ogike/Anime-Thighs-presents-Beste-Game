03025c9fca958c5df17c77f8bd2fe0bd93753c96
ogike <77450926+ogike@users.noreply.github.com>
2021-02-21 20:10:48 +0100

    Particle variation
    Added wall, and seperate player/enemy particles
    Small finetunings
    Particle z axis set in ParticleManager

--- Assets/MyScripts/ParticleManager.cs
+++ Assets/MyScripts/ParticleManager.cs
@@ -6,6 +6,8 @@ public class ParticleManager : MonoBehaviour
 {
     public static ParticleManager Instance { get; private set; } //basically make this a singleton
 
+	public float particlesZLevel = 1.0f;
+
 	private void Awake()
 	{
         Instance = this;
@@ -16,6 +18,7 @@ public class ParticleManager : MonoBehaviour
 	//should be called with StartCoroutine(), because its an ienumerator)
 	public void PlayParticleEffect (GameObject particle, Vector3 position)
 	{
+		position.z = particlesZLevel;
 		StartCoroutine( PlayParticleEffectCoroutine(particle, position) );
 	}
 
@@ -24,6 +27,7 @@ public class ParticleManager : MonoBehaviour
 		GameObject particleObject = GameObject.Instantiate(particle, position, Quaternion.identity);
 		ParticleSystem particleSystem = particleObject.GetComponent<ParticleSystem>();
 
+		//duration for particle should be set to the max particle lifetime in editor!!
 		yield return new WaitForSeconds(particleSystem.main.duration);
 
 		Destroy(particleObject, 0.0f);
